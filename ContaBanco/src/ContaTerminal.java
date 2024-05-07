9949d71191c524b70d421d903e7adee62e527946
Daiane Sena <78742341+DSenaSilva@users.noreply.github.com>
2024-05-07 10:54:40 -0300

    Projeto ContaBanco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    
+   public static void main(String[] args) throws Exception {
+    
+    Scanner scanner= new Scanner(System.in).useLocale(Locale.US);
+
+    System.out.println("Digite o Número da Conta: ");
+    int Numero = scanner.nextInt();
+    System.out.println("Digite a Agencia: ");
+    var Agencia=scanner.next();
+    System.out.println("Digite o Nome do Cliente: ");
+    var NomeCliente=scanner.next();
+    System.out.println("Digite o Saldo: ");
+    double Saldo = scanner.nextDouble();
+
+    System.out.println("Olá!" + NomeCliente + " Sua Conta  "+ Numero + " Agência " + Agencia +" Com o Saldo  " + Saldo + " foi criada com Sucesso!");
+
+       
     }
 }
