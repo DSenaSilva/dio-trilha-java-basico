be63bd632fdd1ee29e8f5e492a75568be6b47ee5
Daiane Sena <78742341+DSenaSilva@users.noreply.github.com>
2024-05-10 11:27:40 -0300

    Alterando Estrutura do Projeto para o nome que foi pedido no desafio

--- DesafioControleFluxo/src/DesafioControleFluxo.java
+++ DesafioControleFluxo/src/DesafioControleFluxo.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Contador {
+public class DesafioControleFluxo {
     public static void main(String[] args) throws Exception {
         Scanner terminal = new Scanner(System.in);
 		System.out.println("Digite o primeiro número: ");
